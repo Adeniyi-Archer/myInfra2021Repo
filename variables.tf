ba27e739dc564a8425ed80bbb883e4206bb7198d
Adeniyi-Archer <adeniyi.ogunsuada1@gmail.com>
2026-06-14 03:07:07 -0500

    Update EC2 key, instance type, and AMI ID

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "myJenkinsEC2Key" 
 }
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t3.micro" 
 }
 
 variable "security_group" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-04680790a315cd58d" 
 }
 variable "versioning" {
     type        = bool
