35eb7f5b91eb8665a31c10a98b4d5a1bdc6acd78
Adeniyi-Archer <adeniyi.ogunsuada1@gmail.com>
2026-06-14 06:25:31 -0500

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,49 +1,52 @@
-resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-04680790a315cd58d"
-      key_name = var.key_name
-      instance_type = "t3.micro"
-      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
-      tags= {
-        Name = "sonar_instance"
-      }
-    }
+# Security Group for SonarQube
+resource "aws_security_group" "sonar_sg" {
+  name        = "security_sonar_group_2022"
+  description = "Security group for SonarQube"
 
- resource "aws_security_group" "sonar-sg-2022" {
-      name        = "security_sonar_group_2022"
-      description = "security group for Sonar"
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-      ingress {
-        from_port   = 9000
-        to_port     = 9000
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-     ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-     # outbound from Sonar server
-      egress {
-        from_port   = 0
-        to_port     = 65535
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
+  tags = {
+    Name = "security_sonar"
+  }
+}
 
-      tags= {
-        Name = "security_sonar"
-      }
-    }
+# Sonar EC2 Instance
+resource "aws_instance" "my_sonar_instance" {
+  ami                    = "ami-04680790a315cd58d"
+  key_name               = var.key_name
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sonar_sg.id]
 
-# Create Elastic IP address for Sonar instance
-resource "aws_eip" "mySonarInstance" {
-  vpc      = true
-  instance = aws_instance.mySonarInstance.id
-tags= {
+  tags = {
+    Name = "sonar_instance"
+  }
+}
+
+# Elastic IP for Sonar (FIXED - removed deprecated vpc)
+resource "aws_eip" "my_sonar_eip" {
+  domain   = "vpc"
+  instance = aws_instance.my_sonar_instance.id
+
+  tags = {
     Name = "sonar_elastic_ip"
   }
 }
